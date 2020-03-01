53197d46d5b424422158b0f6ac7af09b1747d8d4
iulianigas <41596757+iulianigas@users.noreply.github.com>
2020-03-01 11:19:05 +0200

    Primitives Data Types

---
+++ ByteShortIntLong/src/com/iulian/Main.java
@@ -0,0 +1,42 @@
+package com.iulian;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int myValue = 10000;
+
+        int myMinIntValue = Integer.MIN_VALUE;
+        int myMaxIntValue = Integer.MAX_VALUE;
+        System.out.println("Integer Minimum Value = " + myMinIntValue);
+        System.out.println("Integer Maximum Value = " + myMaxIntValue);
+        System.out.println("Busted MAX Value = " + (myMaxIntValue + 1));    //overflow
+        System.out.println("Busted MIN Value = " + (myMinIntValue - 1));    //underflow
+
+        byte myMinByteValue = Byte.MIN_VALUE;
+        byte myMaxByteValue = Byte.MAX_VALUE;
+        System.out.println("Byte Minimum Value = " + myMinByteValue);
+        System.out.println("Byte Maximum Value = " + myMaxByteValue);
+
+        short myMinShortValue = Short.MIN_VALUE;
+        short myMaxShortValue = Short.MAX_VALUE;
+        System.out.println("Short Minimum Value = " + myMinShortValue);
+        System.out.println("Short Maximum Value = " + myMaxShortValue);
+
+        //byte occupies 8 bits
+        //short occupies 16 bits
+        //int occupies 32 bits
+        //long occupies 64 bits
+
+        long myLongValue = 100L;
+        long myMinLongValue = Long.MIN_VALUE;
+        long myMaxLongValue = Long.MAX_VALUE;
+        System.out.println("Long Minimum Value = " + myMinLongValue);
+        System.out.println("Long Maximum Value = " + myMaxLongValue);
+
+        long bigLongLiteralValue = 2_147_483_647L;
+        System.out.println(bigLongLiteralValue);
+
+
+    }
+}
