25228c640eee7d87a9e16772b1273df1e741adef
iulianigas <41596757+iulianigas@users.noreply.github.com>
2020-03-01 21:19:29 +0200

    Primitive data types

---
+++ CharAndBoolean/src/com/iulian/Main.java
@@ -0,0 +1,26 @@
+package com.iulian;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        char myChar = 'D';        //char allows only one character
+
+        //char occupies 16 bits
+
+        char myUnicodeChar = '\u0044';  //unicode to put into character D
+
+        System.out.println(myChar);
+        System.out.println(myUnicodeChar);
+
+        char myCopyrightChar = '\u00A9';
+        System.out.println(myCopyrightChar);
+
+        boolean myTrueValue = true;
+        boolean myFalseValue = false;
+
+
+
+
+    }
+}
